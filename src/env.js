b2f1136429193c32d9430c5554856b8dbc10bc13
Alex Tiron <alexandru.bogdan.tiron10@gmail.com>
2025-04-11 17:58:13 +0300

    Updated env.js with CRON_SECRET

--- src/env.js
+++ src/env.js
@@ -12,6 +12,7 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     REDDIT_AUTH_TOKEN: z.string(),
+    CRON_SECRET: z.string(),
   },
 
   /**
@@ -31,6 +32,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     REDDIT_AUTH_TOKEN: process.env.REDDIT_AUTH_TOKEN,
+    CRON_SECRET: process.env.CRON_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
